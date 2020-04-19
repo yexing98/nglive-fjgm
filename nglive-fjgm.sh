1f526943f3ba8a555a3712ab44ba0ae81cd8c410
yexing98 <62497407+yexing98@users.noreply.github.com>
2020-04-19 13:31:00 +0800

    Update nglive-fjgm.sh

--- nglive-fjgm.sh
+++ nglive-fjgm.sh
@@ -2,26 +2,26 @@
 rm -f /var/run/yum.pid	&&
 yum install -y wget	&&
 yum install -y git	&&
-git clone https://github.com/sivel/speedtest-cli.git	&&
-yum -y install net-tools	&&
-yum -y install vim	&&
-git clone https://github.com/arut/nginx-rtmp-module.git	&&
-wget http://nginx.org/download/nginx-1.8.1.tar.gz	&&
-yum -y install gcc-c++	&&
-yum -y install openssl openssl-devel	&&
-yum -y install pcre pcre-devel	&&
-yum -y install zlib zlib-devel	&&
-yum -y install vsftpd	&&
-tar -zxvf nginx-1.8.1.tar.gz	&&
+git clone https://github.com/sivel/speedtest-cli.git&&
+yum -y install net-tools&&
+yum -y install vim&&
+git clone https://github.com/arut/nginx-rtmp-module.git&&
+wget http://nginx.org/download/nginx-1.8.1.tar.gz&&
+yum -y install gcc-c++&&
+yum -y install openssl openssl-devel&&
+yum -y install pcre pcre-devel&&
+yum -y install zlib zlib-devel&&
+yum -y install vsftpd&&
+tar -zxvf nginx-1.8.1.tar.gz&&
 cd nginx-1.8.1&&
-./configure --prefix=/usr/local/nginx  --add-module=../nginx-rtmp-module  --with-http_ssl_module	&&
-make&&make install	&&
-cd /root &&
-mkdir -p /home/html/hls	&&
-chmod 777 /home/html/hls	&&
-mkdir -p /home/html/record	&&
-chmod 777 /home/html/record	&&
-cp /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.bak	&&
+./configure --prefix=/usr/local/nginx  --add-module=../nginx-rtmp-module  --with-http_ssl_module&&
+make && make install&&
+cd /root&&
+mkdir -p /home/html/hls&&
+chmod 777 /home/html/hls&&
+mkdir -p /home/html/record&&
+chmod 777 /home/html/record&&
+cp /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.bak&&
 rm -f /usr/local/nginx/conf/nginx.conf&&
 cat>/usr/local/nginx/conf/nginx.conf<<EOF
 #user  nobody;
@@ -174,7 +174,7 @@ rtmp{
 }
 
 EOF
-/usr/local/nginx/sbin/nginx -c  /usr/local/nginx/conf/nginx.conf	&&
+/usr/local/nginx/sbin/nginx -c  /usr/local/nginx/conf/nginx.conf&&
 cat>/lib/systemd/system/nginx.service<<EOF
 [Unit]
 Description=nginx
@@ -190,8 +190,8 @@ PrivateTmp=true
 [Install]
 WantedBy=multi-user.target
 EOF
-systemctl enable nginx.service	&&
-mv /usr/local/nginx/html/index.html  /usr/local/nginx/html/index.html.bak	&&
+systemctl enable nginx.service&&
+mv /usr/local/nginx/html/index.html  /usr/local/nginx/html/index.html.bak&&
 cat>/home/html/index.html<<EOF
 <html>
 <head>
@@ -212,7 +212,7 @@ cat>/home/html/index.html<<EOF
 </body>
 </html>
 EOF
-cd /root	&&
+cd /root&&
 cat>>/etc/bashrc<<EOF
 alias cdng="cd /usr/local/nginx/conf"
 alias cdhtml="cd /home/html"
@@ -222,18 +222,17 @@ alias stopng="/usr/local/nginx/sbin/nginx -s stop"
 alias startng="/usr/local/nginx/sbin/nginx -c  /usr/local/nginx/conf/nginx.conf"
 alias vimng="vim /usr/local/nginx/conf/nginx.conf"
 EOF
-firewall-cmd --add-port=1935/tcp --permanent	&&
-firewall-cmd --add-port=81/tcp --permanent	&&
-firewall-cmd --add-service=ftp --permanent	&&
-firewall-cmd --add-service=http --permanent	&&
-firewall-cmd --reload	&&
-systemctl restart firewalld	&&
-echo anonymous_enable=NO>>/etc/vsftpd/vsftpd.conf	&&
-echo chroot_local_user=YES>>/etc/vsftpd/vsftpd.conf	&&
-echo allow_writeable_chroot=YES>>/etc/vsftpd/vsftpd.conf	&&
-systemctl start vsftpd	&&
+firewall-cmd --add-port=1935/tcp --permanent &&
+firewall-cmd --add-port=81/tcp --permanent &&
+firewall-cmd --add-service=ftp --permanent &&
+firewall-cmd --add-service=http --permanent &&
+firewall-cmd --reload &&
+systemctl restart firewalld &&
+echo anonymous_enable=NO>>/etc/vsftpd/vsftpd.conf&&
+echo chroot_local_user=YES>>/etc/vsftpd/vsftpd.conf&&
+echo allow_writeable_chroot=YES>>/etc/vsftpd/vsftpd.conf&&
+systemctl start vsftpd&&
 source  /etc/bashrc	&&
-cd speedtest-cli	&&
+cd speedtest-cli&&
 ./speedtest.py --server 16171	&&
 echo “all install completed” 
-
